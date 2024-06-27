b18dba3903549b3f878c7fe00e96fe0204462332
Hein Htet Aung <>
2024-06-27 09:39:48 +0900

    add project

---
+++ SlotMachine/SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Hein Htet Aung on 2024/06/27.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
